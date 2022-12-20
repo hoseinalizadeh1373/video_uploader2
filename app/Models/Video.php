324cad4deac015fdffbc01210a625a799ed56819
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2022-12-20 12:28:16 +0330

    Initial commit

---
+++ app/Models/Video.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Video extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+    protected $fillable= ['title','desc'];
+
+    public function registerMediaCollections(): void
+    {
+     $this->addMediaCollection('media')
+     ->singleFile();   
+    }
+}
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl" >
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>مدیریت رسانه های تصویری</title>
+
+        <!-- Fonts -->
+        {{-- <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
+        
+        {{-- <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+        {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous"> --}}
+        @vite(['resources/sass/app.scss','resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <body >
+        
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+    
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">لیست ویدیوها</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">آپلود ویدیو</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
