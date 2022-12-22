b6d29514b1102cf4cee7c1383772f5fdd139438f
hoseinalizadeh1373 <hoseinalizadehkhiabani@gmail.com>
2022-12-22 18:21:00 +0330

    ww

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,17 +7,33 @@
 
     <div class="mb-1 w-50">
         <label for="title" class="form-label">عنوان ویدیو</label>
-        <input type="text" class="form-control shadow" id="title" name="title" >
+        <input type="text" class="form-control shadow" id="title" name="title" value="{{old('title')}}">
+        @error('title')
+                            <p class="invalid-feedback d-block">
+                                <strong>{{$message}}</strong>
+                            </p>
+        @enderror
     </div>
 
       <div class="mb-1 w-50">
         <label for="desc" class="form-label">توضیحات</label>
-        <textarea class="form-control shadow" id="desc" name="desc" rows="2"></textarea>
+        <textarea class="form-control shadow" id="desc" name="desc" rows="2">{{old('desc')}}</textarea>
+        @error('desc')
+                            <p class="invalid-feedback d-block">
+                                <strong>{{$message}}</strong>
+                            </p>
+        @enderror
       </div>
 
       <div class="mb-1 w-50">
         <label for="formFile" class="form-label">انتخاب ویدیو</label>
-        <input class="form-control shadow" type="file" name="file">
+        
+        <input id="file_id" class="form-control shadow" type="file" name="file">
+        @error('file')
+                            <p class="invalid-feedback d-block">
+                                <strong>{{$message}}</strong>
+                            </p>
+        @enderror
             <div class="progress">
                 <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
             </div>
